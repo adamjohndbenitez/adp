ed337161b5a67fc862814ade86b937b2e5eb408a
Adam Benitez <abenitez@datex.ca>
2021-09-14 16:02:43 -0400

    [#1] Create and generate spring boot project for adp bill-to-coin changer

---
+++ src/main/java/com/changer/billcoin/BillcoinChangerApplication.java
@@ -0,0 +1,13 @@
+package com.changer.billcoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillcoinChangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillcoinChangerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/changer/billcoin/BillcoinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.changer.billcoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillcoinChangerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
